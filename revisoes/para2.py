e49f4bd920a9cba17434e61f6dd507ac5e5cc153
evelin <macielevelin088@gmail.com>
2024-08-28 18:56:13 -0300

    laço de repetiçao

---
+++ revisoes/para2.py
@@ -0,0 +1,21 @@
+alunos = [
+ "evelin",
+ "dudinha",
+ "larissa"
+"joao",
+"Damiane",
+"Nicollas",
+"vinicius",
+"josefina",
+"antonia",
+"tita"
+] 
+alunos.sort()
+alunos.append("dudinha") 
+alunos .reverse ()
+y = 1 
+
+
+for x in alunos : 
+ print(y,"-",x)
+ y=y+1
